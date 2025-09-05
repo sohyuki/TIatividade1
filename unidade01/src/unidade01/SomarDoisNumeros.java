32e831929cb072a05ad2f5ade134f039b9124aa3
Eduardo S. Silva <unionreader283@gmail.com>
2025-09-04 23:26:20 -0300

    primeiro commit

---
+++ unidade01/src/unidade01/SomarDoisNumeros.java
@@ -0,0 +1,19 @@
+package unidade01;
+
+import java.util.Scanner;
+
+public class SomarDoisNumeros {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num1, num2, soma;
+
+        System.out.println("Digite um número:");
+        num1 = sc.nextInt();
+        System.out.println("Digite outro número:");
+        num2 = sc.nextInt();
+
+        soma = num1 + num2;
+        System.out.println("Soma: " + soma);
+    }
+}
